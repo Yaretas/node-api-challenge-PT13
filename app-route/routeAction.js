834bfb4ad1df0ef8049d5b0dd677b1bde4de91d8
Yarriba Castro <yarribacastro@Yarribas-MacBook-Pro.local>
2020-05-20 22:06:48 -0500

    routeProject completed and tested :)

--- app-route/routeAction.js
+++ app-route/routeAction.js
@@ -6,6 +6,22 @@ const action = require("../data/helpers/actionModel");
 //invoke Router
 const routerA = express.Router();
 
+//Endpoint handlers---------CRUD operations------------------------
+/*Create - POST*/
+routerA.post('/', (req, res) => {
 
+})
+/*Read - GET*/
+routerA.get('/', (req, res) => {
+
+})
+/*Update - PUT*/
+routerA.put('/', (req, res) => {
+
+})
+/*Delete - Delete*/
+routerA.delete('/', (req, res) => {
+
+})
 //Export Router
 module.exports = routerA
